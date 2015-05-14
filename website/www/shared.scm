ef324da4120e4502e682dd2a8c5b01a0f7559233
Ludovic Courtès <ludo@gnu.org>
2015-05-14 12:02:12 +0200

    website: shared: Switch to 0.8.2.
    * website/www/shared.scm (latest-guix-version): Set to 0.8.2.

--- website/www/shared.scm
+++ website/www/shared.scm
@@ -7,7 +7,7 @@
 	    html-page-footer))
 
 (define latest-guix-version
-  (make-parameter "0.8.1"))
+  (make-parameter "0.8.2"))
 
 (define* (html-page-header title #:key (css "article.css"))
   `(head (meta (@ (charset "utf-8")))
